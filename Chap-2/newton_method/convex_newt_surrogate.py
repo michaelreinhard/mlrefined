65da5d21878dcd365a70c27a901be12296bbd38e
Jeremy Watt <jermwatt@gmail.com>
2016-03-03 12:25:22 -0600

    Update convex_newt_surrogate.py

--- Chap-2/newton_method/convex_newt_surrogate.py
+++ Chap-2/newton_method/convex_newt_surrogate.py
@@ -7,6 +7,9 @@
 # The (convex) function here is
 #
 # g(w) = log(1 + exp(w^2))
+#
+# This file pairs with chapter 2 of the textbook "Machine Learning Refined" by Cambridge University Press
+
 from numpy import *
 from matplotlib.pyplot import *
 from pylab import *
