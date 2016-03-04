2e841e28e50e35905377e0e3bd51e7b673ddc4ba
Jeremy Watt <jermwatt@gmail.com>
2016-03-04 16:01:54 -0600

    Update l2reg_logistic.m

--- Chap-3/l2reg_logistic.m
+++ Chap-3/l2reg_logistic.m
@@ -1,5 +1,6 @@
 
-% This file pairs with chapter 3 of the textbook "Machine Learning Refined" published by Cambridge University Press.  
+% This file pairs with chapter 3 of the textbook "Machine Learning Refined" published by Cambridge University Press, 
+% free for download at www.mlrefined.com
 
 function l2reg_logistic()
 
