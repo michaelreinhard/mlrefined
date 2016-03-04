3eaa2f967731c41bbd4cc9bd3ece3bf05e93c5a7
Jeremy Watt <jermwatt@gmail.com>
2016-03-04 15:10:02 -0600

    Update nonconvex_grad_surrogate.py

--- Chap-2/gradient_descent/nonconvex_grad_surrogate.py
+++ Chap-2/gradient_descent/nonconvex_grad_surrogate.py
@@ -8,45 +8,46 @@
 # The (nonconvex) function here is
 #
 # # g(w) = exp(w)*cos(2pi*sin(pi*w))
-#
-# This file pairs with chapter 2 of the textbook "Machine Learning Refined" published by Cambridge University Press
 
-
-from numpy import *
-from matplotlib.pyplot import *
-from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
 import time
 
+# define the objective of our cost function
 def obj(y):
-    z = exp(y)*cos(2*pi*sin(pi*y))
+    z = np.exp(y)*np.cos(2*np.pi*np.sin(np.pi*y))
     return z
+
+# define the gradient of the cost function
 def grad(y):
-    z = exp(y)*cos(2*pi*sin(pi*y)) - 2*pi**2*exp(y)*sin(2*pi*sin(pi*y))*cos(pi*y)
+    z = np.exp(y)*np.cos(2*np.pi*np.sin(np.pi*y)) - 2*np.pi**2*np.exp(y)*np.sin(2*np.pi*np.sin(np.pi*y))*np.cos(np.pi*y)
     return z
+
+# define the linear surrogate at each gradient descent step
 def surrogate(y,x):
     z = obj(y) + grad(y)*(x - y)
     return z
 
-###### ML Algorithm functions ######
+# gradient descent function
 def gradient_descent(w0,alpha):
     w = w0
     obj_path = []
     w_path = []
     w_path.append(w0)
-    obj_path.append(exp(w)*cos(2*pi*sin(pi*w)))
+    obj_path.append(np.exp(w)*np.cos(2*np.pi*np.sin(np.pi*w)))
 
     # start gradient descent loop
     grad = 1
     iter = 1
     max_its = 50
-    while linalg.norm(grad) > 10**(-5) and iter <= max_its:
+    while np.linalg.norm(grad) > 10**(-5) and iter <= max_its:
         # take gradient step
-        grad = exp(w)*cos(2*pi*sin(pi*w)) - 2*pi**2*exp(w)*sin(2*pi*sin(pi*w))*cos(pi*w)
+        grad = np.exp(w)*np.cos(2*np.pi*np.sin(np.pi*w)) - 2*np.pi**2*np.exp(w)*np.sin(2*np.pi*np.sin(np.pi*w))*np.cos(np.pi*w)
         w = w - alpha*grad
 
         # update path containers
         w_path.append(w)
-        obj_path.append(exp(w)*cos(2*pi*sin(pi*w)))
+        obj_path.append(np.exp(w)*np.cos(2*np.pi*np.sin(np.pi*w)))
         iter+= 1
 
     # show final average gradient norm for sanity check
@@ -55,14 +56,14 @@ def gradient_descent(w0,alpha):
     print(s)
     return (w_path,obj_path)
 
-###### plotting functions #######
+# plots the cost function
 def make_function():
     # plot the function
     global fig,ax1
-    fig = figure(facecolor = 'white')
+    fig = plt.figure(facecolor = 'white')
     ax1 = fig.add_subplot(111)
-    s = linspace(0,1.1,2000)
-    t =  exp(s)*cos(2*pi*sin(pi*s))
+    s = np.linspace(0,1.1,2000)
+    t =  np.exp(s)*np.cos(2*np.pi*np.sin(np.pi*s))
     ax1.plot(s,t,'-k',linewidth = 2)
 
     # pretty the figure up
@@ -71,30 +72,31 @@ def make_function():
     ax1.set_xlabel('$w$',fontsize=20,labelpad = 20)
     ax1.set_ylabel('$g(w)$',fontsize=20,rotation = 0,labelpad = 20)
 
+# plot each gradient descent step
 def plot_steps(w_path,g_path):
     #colors for points
-    s = linspace(1/len(g_path),1,len(g_path))
+    s = np.linspace(1/len(g_path),1,len(g_path))
     s.shape = (len(s),1)
-    colorspec = concatenate((s,flipud(s)),1)
-    colorspec = concatenate((colorspec,zeros((len(s),1))),1)
+    colorspec = np.concatenate((s,np.flipud(s)),1)
+    colorspec = np.concatenate((colorspec,np.zeros((len(s),1))),1)
 
     #plot initial point
     ax1.plot(w_path[0],g_path[0],'o',markersize = 9, color = colorspec[0,:], markerfacecolor = colorspec[0,:])
-    draw()
+    plt.draw()
     # ax1.annotate('$w$'+str(0),(w_path[0],-5))
-    t = linspace(-5,g_path[0],100)
-    s = w_path[0]*ones((100))
+    t = np.linspace(-5,g_path[0],100)
+    s = w_path[0]*np.ones((100))
     ax1.plot(s,t,'--k')
-    draw()
+    plt.draw()
     time.sleep(2)
 
     #plot first surrogate and point traveled to
     s_range = .3
-    s = linspace(w_path[0]-s_range,w_path[0]+s_range,10000)
+    s = np.linspace(w_path[0]-s_range,w_path[0]+s_range,10000)
     t = surrogate(w_path[0],s)
     h, = ax1.plot(s,t,'--m')
     r, = ax1.plot(w_path[1],surrogate(w_path[0],w_path[1]),'o',markersize = 6, c = 'k')
-    draw()
+    plt.draw()
 
     for i in range(1,len(g_path)):
         if i < 4:
@@ -102,21 +104,21 @@ def plot_steps(w_path,g_path):
 
                 #plot point
                 ax1.plot(w_path[i],g_path[i],'o',markersize = 9, color = colorspec[i-1,:], markerfacecolor = colorspec[i-1,:])
-                draw()
+                plt.draw()
                 #plot surrogate
                 if i < len(g_path) - 2:
                     time.sleep(1)
                     h.remove()
                     r.remove()
-                    draw()
+                    plt.draw()
                     s_range = .3
-                    s = linspace(w_path[i]-s_range,w_path[i]+s_range,2000)
+                    s = np.linspace(w_path[i]-s_range,w_path[i]+s_range,2000)
                     t = surrogate(w_path[i],s)
                     time.sleep(1)
                     h, = ax1.plot(s,t,'--m')
-                    ind = argmin(abs(s - w_path[i + 1]))
+                    ind = np.argmin(abs(s - w_path[i + 1]))
                     r, = ax1.plot(s[ind],t[ind],'o', markersize = 6, c = 'k')
-                    draw()
+                    plt.draw()
         if i == 4:
             time.sleep(1.5)
             h.remove()
@@ -125,19 +127,20 @@ def plot_steps(w_path,g_path):
         if i > 4: # just plot point so things don't get too cluttered
             time.sleep(0.01)
             ax1.plot(w_path[i],g_path[i],'o',markersize = 9, color = colorspec[i-1,:], markerfacecolor = colorspec[i-1,:])
-            draw()
+            plt.draw()
         if i == len(g_path) - 1:
             ax1.annotate('$w$'+str(i),(w_path[i],-5))
-            t = linspace(-5,g_path[i],100)
-            s = w_path[i]*ones((100))
+            t = np.linspace(-5,g_path[i],100)
+            s = w_path[i]*np.ones((100))
             ax1.plot(s,t,'--k')
 
-    show(True)
+    plt.show(True)
 
+# main
 def main():
     alpha = 2*10**-3                             # step size
     make_function()                              # plot objective function
-    pts = matrix(ginput(1))
+    pts = np.matrix(plt.ginput(1))
     x = pts[:,0]
     w0 = float(x[0])
     w_path,obj_path = gradient_descent(w0,alpha) # perform gradient descent
